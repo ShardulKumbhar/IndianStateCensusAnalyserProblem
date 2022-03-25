1c5ee39556965bff2eed028f91b8bc5b65375805
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-25 23:11:13 +0530

    if incorrect return a custom exception

--- src/test/java/IndianStateCensusAnalyserTest.java
+++ src/test/java/IndianStateCensusAnalyserTest.java
@@ -1,6 +1,7 @@
 import static org.junit.Assert.*;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -21,7 +22,10 @@ public class IndianStateCensusAnalyserTest {
 	 */
 	private static String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\shard\\eclipse-workspace\\Advance java"
 			+ "\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\src\\Resource\\IndiaStateCensusData.csv";
-
+	private static final String WRONG_CSV_FILE_PATH ="C:\\Users\\shard\\eclipse-workspace\\Advance java"
+			+ "\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\src\\Resource\\IndiaStateCod.txt";
+			
+			
 	/**
 	 * 2.method created for given Census CSV File returns correct number of entries
 	 * otherwise throws exception....
@@ -41,5 +45,26 @@ public class IndianStateCensusAnalyserTest {
 		}
 		Assert.assertEquals(29, count);
 	}
+
+	public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+    	List<StateCensusCSV> users = new ArrayList<StateCensusCSV>();
+		String line = "";
+		BufferedReader reader;
+		int count1 = 0;
+		try {
+			reader = new BufferedReader(new FileReader(WRONG_CSV_FILE_PATH));
+			reader.readLine();
+			while ((line = reader.readLine()) != null) {
+				count1 += 1;
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found");
+		} catch (IOException e) {
+			System.out.println("IOException");
+		}
+		Assert.assertEquals(29,count1);
+		
+    }
+
 
 }
