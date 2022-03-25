c8622b85494e2c2e0281bf398369e29e64a0a36f
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-25 18:06:54 +0530

    reading csv file

---
+++ src/main/java/censuscsv/StateCensusCSV.java
@@ -0,0 +1,58 @@
+package censuscsv;
+
+import com.opencsv.bean.CsvBindByName;
+
+public class StateCensusCSV {
+	
+	/**
+	 * 1.variable decration
+	 * 2.Generating Getters/Setters
+	 */
+	
+	/**
+	 * 
+	 * 1.variable decration
+	 */
+	
+	private String state;
+	private long pop;
+	private int area;
+	private int density;
+
+	/**
+	 * 2.Generating Getters/Setters
+	 */
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public long getPop() {
+		return pop;
+	}
+
+	public void setPop(long pop) {
+		this.pop = pop;
+	}
+
+	public int getArea() {
+		return area;
+	}
+
+	public void setArea(int area) {
+		this.area = area;
+	}
+
+	public int getDensity() {
+		return density;
+	}
+
+	public void setDensity(int density) {
+		this.density = density;
+	}
+
+}
