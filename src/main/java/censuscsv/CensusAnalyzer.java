5403c1e190bca95737fbf03f0847334527ab4608
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-28 17:53:51 +0530

    added to load indian census data

---
+++ src/main/java/censuscsv/CensusAnalyzer.java
@@ -0,0 +1,76 @@
+package censuscsv;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Iterator;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+
+			/**
+			 * 1. try catch to handle exception
+			 * 2. to read the file path
+			 * 3.class csvToBeanBuilder Class CsvToBeanBuilder<T> This class makes it
+			 * 4.itrator to itrate every string value iteator dosent occupies memmory
+			 * 5. has next to itrate to next string
+			 * 6.to take a count in number format
+			 * 7.enum exception type
+			 * @author shardul
+			 *
+			 */
+			public class CensusAnalyzer {
+				public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
+					/*
+					 * 1. try catch to handle exception
+					 */
+					try {
+						/*
+						 * 2. to read the file path
+						 */
+						Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));
+
+						/*
+						 * 3.class csvToBeanBuilder Class CsvToBeanBuilder<T> This class makes it
+						 * possible to bypass all the intermediate steps and classes in setting up to
+						 * read from a CSV source to a list of beans.
+						 */
+						CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<IndiaCensusCSV>(
+								reader);
+						csvToBeanBuilder.withType(IndiaCensusCSV.class);
+						csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+						CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+
+						/*
+						 * 4.itrator to itrate every string value iteator dosent occupies memmory
+						 */
+						Iterator<IndiaCensusCSV> censusCSVIterator = csvToBean.iterator();
+						int numberOfEntries = 0;
+						/**
+						 * 5. has next to itrate to next string
+						 */
+						while (censusCSVIterator.hasNext()) {
+							/*
+							 * 6.to take a count in number format
+							 */
+							numberOfEntries++;
+							IndiaCensusCSV censusData = censusCSVIterator.next();
+						}
+						return numberOfEntries;
+					} catch (IOException e)
+					/*
+					 * 7.enum exception type
+					 */
+					{
+						throw new CensusAnalyserException(e.getMessage(),
+								CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
+					} catch (RuntimeException e)
+
+					{
+						throw new CensusAnalyserException(e.getMessage(),
+								CensusAnalyserException.ExceptionType.INVALID_FILE_TYPE_OR_DELIMITER_OR_HEADER);
+					}
+				}
+
+			}
--- src/test/java/IndianStateCensusAnalyserTest.java
+++ src/test/java/IndianStateCensusAnalyserTest.java
@@ -8,63 +8,29 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
+
+import censuscsv.CensusAnalyserException;
+import censuscsv.CensusAnalyzer;
 import censuscsv.StateCensusCSV;
 
 public class IndianStateCensusAnalyserTest {
     /**
-     * steps
-     * 1.checking file path of csv file
-     * 2.method created for given Census CSV File returns correct number of entries
-	 * otherwise throws exception....
+     * File path
      */
-	/*
-	 * 1.checking file path of csv file
-	 */
-	private static String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\shard\\eclipse-workspace\\Advance java"
+	 private static final String INDIA_CENSUS_CSV_FILE_PATH= "C:\\Users\\shard\\eclipse-workspace\\Advance java"
 			+ "\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\src\\Resource\\IndiaStateCensusData.csv";
-	private static final String WRONG_CSV_FILE_PATH ="C:\\Users\\shard\\eclipse-workspace\\Advance java"
-			+ "\\IndianStateCensusAnalyserProblem\\IndianStateCensusAnalyserProblem\\src\\Resource\\IndiaStateCod.txt";
+	
 			
-			
-	/**
-	 * 2.method created for given Census CSV File returns correct number of entries
-	 * otherwise throws exception....
-	 * 
-	 * @throws IOException
-	 */
-	@Test
-	public void givenIndianCensusCSVFileReturnsCorrectRecords() throws IOException {
-
-		List<StateCensusCSV> users = new ArrayList<StateCensusCSV>();
-		String line = "";
-		BufferedReader reader = new BufferedReader(new FileReader(INDIA_CENSUS_CSV_FILE_PATH));
-		reader.readLine();
-		int count = 0;
-		while ((line = reader.readLine()) != null) {
-			count += 1;
-		}
-		Assert.assertEquals(29, count);
-	}
-
-	public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
-    	List<StateCensusCSV> users = new ArrayList<StateCensusCSV>();
-		String line = "";
-		BufferedReader reader;
-		int count1 = 0;
-		try {
-			reader = new BufferedReader(new FileReader(WRONG_CSV_FILE_PATH));
-			reader.readLine();
-			while ((line = reader.readLine()) != null) {
-				count1 += 1;
-			}
-		} catch (FileNotFoundException e) {
-			System.out.println("File Not Found");
-		} catch (IOException e) {
-			System.out.println("IOException");
-		}
-		Assert.assertEquals(29,count1);
-		
+	  //Test Case 1.1
+    @Test
+    public void givenIndianCensusDataCSVFileReturnsCorrectRecords()
+    {
+        try
+        {
+            CensusAnalyzer censusAnalyser = new CensusAnalyzer();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        }
+        catch (CensusAnalyserException e) { }
     }
-
-
 }
